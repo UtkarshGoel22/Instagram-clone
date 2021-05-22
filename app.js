f555d94306427df5a1e28b718c6a542914699283
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2021-05-22 12:26:41 +0530

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 const {MONGOURI} = require("./config/keys");
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true});
@@ -20,14 +20,14 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV == "production") {
+if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
     const path = require('path')
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 app.listen(PORT, function() {
-    console.log("Server is Running on port 5000.");
+    console.log("Server is Running on", PORT);
 });
